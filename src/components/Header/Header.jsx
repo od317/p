2de1fc48ef96bf4392266e32a023397dd2b12d61
unknown <osamadrbas31@gmail.com>
2024-01-03 16:05:51 +0200

    New

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React, { useRef, useState,useEffect,useLayoutEffect} from 'react'
 import Blobs from '../blobs/Blobs'
 import { useWindowScroll } from "@uidotdev/usehooks"
-import HeaderText from './HeaderText.Jsx'
+// import HeaderText from './HeaderText.Jsx'
 
 
 function Header({p}) {
     
     return (<>
 
-      <HeaderText p={p}></HeaderText> 
+      {/* <HeaderText p={p}></HeaderText>  */}
       </>
     )
 }
